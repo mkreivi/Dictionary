58465caa8767067b3a3dcf4eba903b3dcbe6caae
Martina Kreivi <martinakreivi@gmail.com>
2023-11-24 14:35:20 +0100

    Set up

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/header";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <Header />;
+}
+
+export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Dictionary</h1>
+    </header>
+  );
+}
+
+export default Header;
