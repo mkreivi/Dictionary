103587617be2f0d53aee11b7e124ada4d3975f4a
Martina Kreivi <martinakreivi@gmail.com>
2023-11-27 13:08:38 +0100

    removed unused file

--- src/apiService.tsx
+++ src/apiService.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 
-//const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-
 function useApiService() {
   const [wordData, setWordData] = useState<any>(null);
   const [error, setError] = useState<string | null>(null);
@@ -16,10 +14,10 @@ function useApiService() {
       }
       const data = await response.json();
       setWordData(data[0] || null);
-      setError(null); // This line should be placed here
+      setError(null);
     } catch (error) {
       console.error("Error fetching definition:", error);
-      setError((error as Error).message); // Move this line here and cast 'error' to 'Error'
+      setError((error as Error).message);
       throw error;
     }
   };
